01102ba73031d14de7d70f541750f513937a1d1b
Johanna Liu <johanna.liu@mths.ca>
2022-09-22 15:23:18 +0000

    added

--- circle.py
+++ circle.py
@@ -10,11 +10,14 @@ import math
 
 def main():
     # this function calculates the area and perimeter
+    area = math.pi * 15**2
+    perimeter = 2 * math.pi * 15
 
-    print("If a circle has a radius of 12 cm: ")
+    print("If a circle has a radius of 15 cm: ")
     print("")
-    print("Area is {} mm².".format(math.pi * 12**2))
-    print("Perimeter is {} mm.".format(2 * math.pi * 12))
+    print("Area = πr²")
+    print("Area is {}mm².".format(round(area, 2)))
+    print("Perimeter is {}mm.".format(round(perimeter, 2)))
     print("")
     print("Done.")
 
